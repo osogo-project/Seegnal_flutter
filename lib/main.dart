0e6d2343fdf4138e7b002ef8a48a90aab831bea4
lrwn <kleedo19@naver.com>
2023-04-13 22:49:10 +0900

    picture is saved in getTemporaryDirectory(). When App is restarted, directory is cleared. If we want to save permanently, we can use 'path_provider' package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,102 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const CameraApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CameraApp extends StatefulWidget {
+  const CameraApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<CameraApp> createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCameraController();
+  }
+
+  void initializeCameraController() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    _controller = CameraController(_cameras[0], ResolutionPreset.max);
+    _initializeControllerFuture = _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {}); // Force refresh after initialization
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        body: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller);
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.camera_alt),
+          onPressed: () async {
+            try {
+              await _initializeControllerFuture;
+              final path = join(
+                (await getTemporaryDirectory()).path,
+                '${DateTime.now()}.png',
+              );
+              await _controller.takePicture();
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CameraPreviewScreen(imagePath: path),
+                ),
+              );
+            } catch (e) {
+              print(e);
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
 
+class CameraPreviewScreen extends StatelessWidget {
+  final String imagePath;
+
+  const CameraPreviewScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Preview')),
+      body: Image.file(File(imagePath)),
     );
   }
 }
